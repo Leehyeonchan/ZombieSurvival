6999b8a503e31c5c99487e19bb6b37ca296445c7
tladud123 <guscksdl3460@gmail.com>
2022-04-11 15:37:29 +0900

    20220411_zombie

---
+++ Zombie/Assets/02.Scripts/GameMamager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMamager : MonoBehaviour
+{
+    public static GameMamager instance;
+
+        public bool isGameover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
